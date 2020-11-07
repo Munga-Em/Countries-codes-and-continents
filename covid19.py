32700096610c367babe9f51970c6541ec72888e5
Munga-Em <62285612+Munga-Em@users.noreply.github.com>
2020-11-07 20:51:26 +0300

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -399,7 +399,7 @@ with col10:
     st.markdown("<p style='text-align: left; color: white;'>Showing top 10 counties only</p>",
                 unsafe_allow_html=True)
     fig = px.bar(data5, x='total_cases', y='County', orientation='h', hover_name= None, color='County', hover_data = {'month': False, 'period': data5['Month_yr']},
-                 animation_frame='month', animation_group='County', range_x=[0,county_max])
+                 animation_frame='month', animation_group='County', range_x=[0,county_max], range_y=[0,10])
     fig.layout.update(yaxis=dict(titlefont=dict(size=14),color = '#FFFFFF', zeroline=False, showgrid=False),
                       xaxis=dict(titlefont=dict(size=14), tickformat=',.0f', color = '#FFFFFF', autorange=False, 
                                  range=[0, county_max], showline=False, zeroline=False, showgrid=False),
