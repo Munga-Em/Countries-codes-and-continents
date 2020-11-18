277d952ec2ceb5a78b7421f6c67ded78f50186a4
Munga-Em <62285612+Munga-Em@users.noreply.github.com>
2020-11-18 12:11:17 +0300

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -441,7 +441,7 @@ with col11:
 
 
 # In[19]:
-st.markdown("<p style='text-align: left; color: white;'>Click the checkbox to show world map of active cases over time</p>",
+st.markdown("<p style='text-align: left; color: white;'>View world map of active cases over time</p>",
                 unsafe_allow_html=True)
 if st.checkbox('Show map'):
     fig = px.choropleth(world_cases,locations='Country', locationmode='country names', color='Active cases',
